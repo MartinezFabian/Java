268c3c8ad655de1dab81706fd15949428a0983f1
MartinezFabian <FabianMartinezDev@gmail.com>
2024-04-24 18:29:24 -0300

    Add Variables.java

---
+++ src/main/java/org/example/helloworld/Variables.java
@@ -0,0 +1,15 @@
+package org.example.helloworld;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Fabian";
+        char firstLetter = 'F';
+        short age = 22;
+        float expectedSalary = 500.99f;
+        boolean programmer = true;
+
+        System.out.println("\nName: " + name + "\nFirstLetter: " + firstLetter + "\nAge: " + age
+                + "\nExpected Salary: " + expectedSalary + "\nProgrammer: " + programmer);
+    }
+}
+    
