df6224d44b24407fc40e21e7521f22cbd1caaacf
MartinezFabian <FabianMartinezDev@gmail.com>
2024-04-24 19:56:25 -0300

    Add ifelse/TestIfInLine.java

---
+++ src/main/java/ifelse/TestIfInLine.java
@@ -0,0 +1,17 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class TestIfInLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        String result = number % 2 == 0 ? " is even." : " is odd.";
+
+        System.out.println("The number " + number + result);
+    }
+}
