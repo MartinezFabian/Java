dd386083c659a3af2b84aeb19d5110fe4ba74a9a
MartinezFabian <FabianMartinezDev@gmail.com>
2024-04-26 17:51:28 -0300

    Add Main.java and Date.java for a basic example class

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+
+        date.setDay(26);
+        date.setMonth(4);
+        date.setYear(2024);
+
+        System.out.println(date.getDay() + "/" + date.getMonth() + "/" + date.getYear());
+    }
+}
